26da6bc424b2b1653f8d685e154041e62545e189
im-natsu <n.haga@infomart.co.jp>
2025-02-27 16:03:43 +0900

    本番用

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-module.exports = {
-  async rewrites() {
-    return [
-      {//ローカル用
-        // source: '/api/:path*',
-        // destination: 'http://localhost:8080/api/:path*', // バックエンドのURLに転送
-      },
-    ];
-  },
-};
+// module.exports = {
+//   async rewrites() {
+//     return [
+//       {//ローカル用
+//         // source: '/api/:path*',
+//         // destination: 'http://localhost:8080/api/:path*', // バックエンドのURLに転送
+//       },
+//     ];
+//   },
+// };
